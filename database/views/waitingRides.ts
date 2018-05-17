55539566964d69e3de40c0895c2e2de73aa96f8f
elong <elongliks@gmail.com>
2018-05-17 23:55:40 +0300

    Changed coordinate system on waiting rides function.

--- database/views/waitingRides.ts
+++ database/views/waitingRides.ts
@@ -1,5 +1,9 @@
 import executeQuery from '../helpers/executeQuery';
 export default async function waitingRides() {
   const query = { text: 'SELECT * FROM waiting_rides;' };
-  return await executeQuery(query);
+  return (await executeQuery(query)).map(ride => ({
+    ...ride,
+    startPoint: { longitude: ride.startPoint.x, latitude: ride.endPoint.y },
+    endPoint: { longitude: ride.endPoint.x, latitude: ride.endPoint.y }
+  }));
 }
