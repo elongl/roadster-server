6696d6d68705217453df2da572957954077952c8
elong <elongliks@gmail.com>
2018-05-30 17:24:40 +0300

    Updated remote database credentials.

--- database/helpers/executeQuery.ts
+++ database/helpers/executeQuery.ts
@@ -1,7 +1,12 @@
 import { Client } from 'pg';
 import { toCamelCase } from '../../utils/mapObject';
 export default async function executeQuery(query: Query) {
-  const client = new Client({ database: 'roadster' });
+  const client = new Client({
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE || 'roadster',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+  });
   await client.connect();
   const res = await client.query(query).finally(() => client.end());
   return res.rows.map(row => toCamelCase(row));
