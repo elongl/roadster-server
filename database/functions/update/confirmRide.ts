e5337cf5929c3207103f2da23ad247a9b1d16866
elong <elongliks@gmail.com>
2018-06-01 00:51:03 +0300

    Added Travis CI.

--- database/functions/update/confirmRide.ts
+++ database/functions/update/confirmRide.ts
@@ -2,7 +2,7 @@ import executeQuery from '../../helpers/executeQuery';
 
 export default async function confirmRide(rideId: number) {
   const query = {
-    text: `UPDATE rides SET status = 'in progress' WHERE id=$1`,
+    text: `UPDATE rides SET status = 'live' WHERE id=$1`,
     values: [rideId]
   };
   await executeQuery(query);
