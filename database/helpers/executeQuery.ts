4a9a9d41bbab4de277d918d00cea6e0e1928addf
elong <elongliks@gmail.com>
2018-06-01 14:47:41 +0300

    Fixed database credentials.

--- database/helpers/executeQuery.ts
+++ database/helpers/executeQuery.ts
@@ -2,17 +2,13 @@ import { Client } from 'pg';
 import { toCamelCase } from '../../utils/mapObject';
 export default async function executeQuery(query: Query) {
   const { DB_HOST, DB_DATABASE, DB_USER, DB_PASSWORD, DB_SSL, NODE_ENV } = process.env;
-  const developmentClient = { database: 'roadster' };
-  const productionClient = {
+  const client = new Client({
     host: DB_HOST,
-    database: DB_DATABASE,
+    database: DB_DATABASE || 'roadster',
     user: DB_USER,
     password: DB_PASSWORD,
-    ssl: Boolean(DB_SSL)
-  };
-  const client = new Client(
-    NODE_ENV === 'production' ? productionClient : developmentClient
-  );
+    ssl: Boolean(DB_SSL) || false
+  });
 
   await client.connect();
   const res = await client.query(query);
